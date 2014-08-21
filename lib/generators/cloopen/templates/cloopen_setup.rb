58655426f910ec1c9f6e41443abc7c1896f69c81
xiaoronglv <xiaoronglv@hotmail.com>
2014-08-21 13:09:39 +0800

    bugfix: setup

--- lib/generators/cloopen/templates/cloopen_setup.rb
+++ lib/generators/cloopen/templates/cloopen_setup.rb
@@ -1,8 +1,14 @@
 ##### 云通讯的配置文件 ####
 
-Cloopen.setup do |config|
-  config.account_sid = "YOUR_ACCOUNT_SID"
-  config.auth_token  = "YOUR_AUTH_TOKEN"
-  config.app_id = "YOUR_APP_ID"
-  config.env = "production"
-end
+Cloopen.account_sid = "YOUR_ACCOUNT_SID"
+Cloopen.auth_token  = "YOUR_AUTH_TOKEN"
+Cloopen.app_id = "YOUR_APP_ID"
+Cloopen.env = "production"
+
+
+##### demo ####
+
+# Cloopen.account_sid = "8a48b55147ae1bf00147af6d8b9003c9"
+# Cloopen.auth_token  = "7ade86d4cc4a458eb12680a0e58df487"
+# Cloopen.app_id = "8a48b55147ae1bf00147af6dbc8a03cb"
+# Cloopen.env = Rail.env
