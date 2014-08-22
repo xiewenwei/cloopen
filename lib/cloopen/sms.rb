1a54d8dd7fca1dc7ba1388aa540cedefb1015b43
xiaoronglv <xiaoronglv@hotmail.com>
2014-08-22 14:42:05 +0800

    convert to module

--- lib/cloopen/sms.rb
+++ lib/cloopen/sms.rb
@@ -3,48 +3,38 @@ require 'uri'
 require 'json'
 
 module Cloopen
-  class SMS
-    def initialize(cellphone, tmpl_id, contents)
-      @cellphone   = cellphone
-      @template_id = tmpl_id
-      @contents    = contents
-      @sig_parameter, @authorization = Sign.generate_sig_and_auth
-    end
+  module SMS
+    # 发送短信
+    # Cloopen::SMS.deliver(18668189882, 3127, ["w1n2ty"])
+    # 手机号: 18668189882
+    # 模板 id: 3127
+    # 模板变量替换值: ["937218"]
+    def self.deliver(cellphone, template_id, contents)
+      sig_parameter, authorization = Sign.generate_sig_and_auth
 
-    def deliver
       if Cloopen.env == "production"
         base_uri = "https://app.cloopen.com:8883/2013-12-26/Accounts/"
       else
         base_uri = "https://sandboxapp.cloopen.com:8883/2013-12-26/Accounts/"
       end
 
-      sms_url = "#{base_uri}#{Cloopen.account_sid}/SMS/TemplateSMS?sig=#{@sig_parameter}"
+      sms_url = "#{base_uri}#{Cloopen.account_sid}/SMS/TemplateSMS?sig=#{sig_parameter}"
 
       payload = {
-        to:         @cellphone,
+        to:         cellphone,
         appId:      Cloopen.app_id,
-        templateId: @template_id,
-        datas:      @contents
+        templateId: template_id,
+        datas:      contents
       }.to_json
 
       header = {
         "Content-type"  => "application/json;charset=utf-8",
         "Accept"        => "application/json",
-        "Authorization" => @authorization
+        "Authorization" => authorization
       }
 
       res = JSON.parse(RestClient.post sms_url, payload, header)
       [res["statusCode"], res["statusMsg"]]
-    end
-
-    # 发送短信
-    # Cloopen::SMS.deliver(18668189882, 3127, ["w1n2ty"])
-    # 手机号: 18668189882
-    # 模板 id: 3127
-    # 模板变量替换值: ["w1n2ty"]
-    def self.deliver(cellphone, tmpl_id, contents)
-      sms = new(cellphone, tmpl_id, contents)
-      sms.deliver
     end
   end
 end
