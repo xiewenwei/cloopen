67046e0d7dcbc84a099ad7394787b5c6b6c17c83
debbbbie <debbbbie@163.com>
2015-06-06 11:54:31 +0800

    Update cloopen_setup.rb

--- lib/generators/cloopen/templates/cloopen_setup.rb
+++ lib/generators/cloopen/templates/cloopen_setup.rb
@@ -2,13 +2,13 @@
 
 Cloopen.account_sid = "YOUR_ACCOUNT_SID"
 Cloopen.auth_token  = "YOUR_AUTH_TOKEN"
-Cloopen.app_id = "YOUR_APP_ID"
-Cloopen.env = "production"
+Cloopen.app_id      = "YOUR_APP_ID"
+Cloopen.env         = "production"
 
 
 ##### demo ####
 
 # Cloopen.account_sid = "8a48b55147ae1bf00147af6d8b9003c9"
 # Cloopen.auth_token  = "7ade86d4cc4a458eb12680a0e58df487"
-# Cloopen.app_id = "8a48b55147ae1bf00147af6dbc8a03cb"
-# Cloopen.env = Rail.env
+# Cloopen.app_id      = "8a48b55147ae1bf00147af6dbc8a03cb"
+# Cloopen.env         = Rail.env
